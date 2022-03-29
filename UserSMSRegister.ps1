c1753d04dff987fcbe1ef3cce32d91f7af30d660
Enes Şirin <45595367+enessirin@users.noreply.github.com>
2022-03-29 14:08:30 +0300

    Add files via upload

---
+++ UserSMSRegister.ps1
@@ -0,0 +1,29 @@
+﻿try{Add-PSSnapin FIMAutomation -ErrorAction Stop
+Write-Host "FIMAutomation Module is imported" -ForegroundColor Green}
+catch{Write-Host "FIMAutomation Module could not be imported" -ForegroundColor Red
+break}
+try{Import-Module ActiveDirectory -ErrorAction Stop
+Write-Host "ActiveDirectory Module is imported" -ForegroundColor Green}
+catch{Write-Host "ActiveDirectory Module could not be imported" -ForegroundColor Red
+break}
+param(
+     [Parameter(Mandatory=$true)]
+     [string]$DomainName
+ )
+$adusers = Get-ADUser -Filter * -Properties SamAccountName,MobilePhone
+$adusers  | ForEach-Object {
+
+$SamAccountName = $_.SamAccountName
+$Mobile = $_.MobilePhone
+$SamAccountName = $DomainName+"\"+$SamAccountName
+$template = Get-AuthenticationWorkflowRegistrationTemplate -AuthenticationWorkflowName "Password Reset AuthN Workflow"
+$custom = $template.Clone()
+$custom.GateRegistrationTemplates.Data.Value = $Mobile
+try{
+Register-AuthenticationWorkflow -UserName $SamAccountName -AuthenticationWorkflowRegistrationTemplate $custom
+Write-Host $SamAccountName "is registered." -ForegroundColor Green
+}
+catch{
+Write-Host $SamAccountName "is not registered." -ForegroundColor Red
+}
+}
